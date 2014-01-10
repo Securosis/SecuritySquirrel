a154bc1a31cf7e87eff533759a24d7e12aecf54f
rmogull <rmogull@securosis.com>
2014-01-10 15:47:51 -0700

    Added most of snapshot method, still need to finish

--- securitysquirrel2.rb
+++ securitysquirrel2.rb
@@ -12,7 +12,11 @@ require "json"
 
 # class for chef integration
 class ConfigManagement
+  # This class integrates with Chef for configuration management. Right now it only has one method. Need to move initialization to an init method when I add more functionality. 
   def analyze
+    # This method polls EC2 and polls Chef to identify any unmanaged instances.
+    # Right now it uses the instance name since there is a bug in the Ridley SDK that limits pulling alternate attribures, but plan is to fix that soon
+    
     # Load configuration and credentials from a JSON file
 
     configfile = File.read('creds.json')
@@ -73,7 +77,7 @@ class IncidentResponse
     @instance_id = instance_id
     
     # Load configuration and credentials from a JSON file
-    configfile = File.read('creds.json')
+    configfile = File.read('config.json')
     config = JSON.parse(configfile)
     
     # Set AWS config
@@ -93,12 +97,43 @@ class IncidentResponse
     @@ec22 = Aws::EC2.new
     @@ec22 = Aws.ec2
   end
+  
   def quarantine
+    # this method moves the provided instance into the Quarantine security group defined in the config file.
     puts ""
-    puts "new stuff"
-    puts "Quarantining #{@instance_id}"
+    puts "Quarantining #{@instance_id}..."
    quarantine = @@ec22.modify_instance_attribute(instance_id: "#{@instance_id}", groups: ["#{@@QuarantineGroup}"])
-   puts "#{@instance_id} moved to the Quarantine security group."
+   puts "#{@instance_id} moved to the Quarantine security group from your configuration settings."
+   end
+  
+  def tag
+    # this method adds an "status => IR" tag to the instance.
+    # If you properly configure your IAM policies, this will move ownership fo the instance to the security
+    # team and isolate it so no one else can terminate/stop/modify/etc.
+    puts "Tagging instance with 'IR'..."
+    tag = @@ec22.create_tags(resources: ["#{@instance_id}"], tags: [
+    {
+      key: "SecurityStatus",
+      value: "IR",
+    },
+  ],)
+  puts "Instance tagged and IAM restrictions applied."
+  end
+  
+  def snapshot
+    # This method determines the volume IDs for the instance, then creates snapshots of those def volumes(args)
+    # Get the instance details for the instance
+    instance_details = @@ec22.describe_instances(
+      instance_ids: ["#{@instance_id}"],
+    )
+    # find the attached block devices, then the ebs volumes, then the volume ID for each EBS volume. This involves walking the response tree.
+    # There is probably a better way to do this in Ruby, but I'm still learning.
+    block_devices = instance_details.reservations.first.instances.first.block_device_mappings
+    ebs = block_devices.map(&:ebs)
+    volumes = ebs.map(&:volume_id)
+    volumes.each do |vol|
+      puts vol
+    end
     
   end
 end
@@ -111,7 +146,7 @@ puts "\e[H\e[2J"
 puts "Welcome to SecuritySquirrel. Please select an action:"
 puts ""
 puts "1. Identify all unmanaged instances"
-puts "2. Initiate Quarantine and Forensics on an instance"
+puts "2. Initiate Full Quarantine and Forensics on an instance"
 puts ""
 print "Select: "
 menuselect = gets.chomp
@@ -124,5 +159,7 @@ elsif menuselect == "2"
   instance_id = gets.chomp
   incident_response = IncidentResponse.new(instance_id)
   incident_response.quarantine
+  incident_response.tag
+  incident_response.snapshot
 end
 
